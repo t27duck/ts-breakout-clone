4fbad352f6afb44ed10b49251196fe999f766c93
Tony Drake <t27duck@gmail.com>
2022-05-14 14:27:38 -0400

    Init game scaffolding

--- src/view/CanvasView.ts
+++ src/view/CanvasView.ts
@@ -5,7 +5,7 @@ import { Ball } from "../sprites/Ball";
 
 
 export class CanvasView {
-  private canvas: HTMLCanvasElement;
+  canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D | null;
   private scoreDisplay: HTMLObjectElement | null;
   private start: HTMLObjectElement | null;
